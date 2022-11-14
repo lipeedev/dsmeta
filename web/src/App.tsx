efcd6d7bfcdbd13e66194df4816cc5a22853d347
1Leep <anamariafakezaum@gmail.com>
2022-11-14 19:18:10 -0300

    feat: first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+
+export function App() {
+
+  return (
+    <h1>Hello World</h1>
+  );
+
+}
+
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx', './index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
