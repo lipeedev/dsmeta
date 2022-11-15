a609d23dfa45f7c3940c4a58cf8c1389eef1a5aa
1Leep <anamariafakezaum@gmail.com>
2022-11-14 23:30:33 -0300

    feat: add react useState hook to manage DatePicker info

--- web/src/components/FormControl.tsx
+++ web/src/components/FormControl.tsx
@@ -1,13 +1,19 @@
+import { Dispatch, SetStateAction } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-export function FormControl() {
+interface FormControlProps {
+    date: Date;
+    setDate: Dispatch<SetStateAction<Date>>;
+}
+
+export function FormControl({ date, setDate }: FormControlProps) {
     return (
         <div>
             <div className="mb-4 max-w-[300px]">
                 <DatePicker
-                    selected={new Date()}
-                    onChange={(date: Date) => { }}
+                    selected={date}
+                    onChange={(date: Date) => setDate(date)}
                     className="w-full h-[46px] bg-[#1b2531] border border-[#384459] rounded-md text-[#9aaabe] pt-0 px-5 text-lg"
                     dateFormat="dd/MM/yyyy"
                 />
--- web/src/components/SalesCard.tsx
+++ web/src/components/SalesCard.tsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import { FormControl } from "./FormControl"
 import { SalesTableTBody } from "./SalesTableTBody";
 import { SalesTableTHead } from "./SalesTableTHead"
 
 export function SalesCard() {
+
+    const ONE_YEAR_IN_MILISSECONDS = 31536e6;
+
+    const [minDate, setMinDate] = useState(new Date(Date.now() - ONE_YEAR_IN_MILISSECONDS));
+    const [maxDate, setMaxDate] = useState(new Date());
+
     return (
         <div className="bg-[#283142] rounded-lg pt-[30px] px-5 md:p-9">
             <h2 className="text-[#676fff] text-2xl mb-5">Vendas</h2>
 
-            <FormControl />
-            <FormControl />
+            <FormControl date={minDate} setDate={setMinDate} />
+            <FormControl date={maxDate} setDate={setMaxDate}/>
 
             <div>
                 <table className="w-full border-spacing-0 border-collapse">
