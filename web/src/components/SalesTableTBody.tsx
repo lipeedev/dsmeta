76cc82a618b99f0b83e71cff164401882cd6c456
1Leep <anamariafakezaum@gmail.com>
2022-11-14 22:13:39 -0300

    feat: add sales table body component

---
+++ web/src/components/SalesTableTBody.tsx
@@ -0,0 +1,21 @@
+import { NotificationButton } from "./NotificationButton";
+
+export function SalesTableTBody() {
+    return (
+        <tbody className="text-sm font-normal text-center text-[#cfcfcf] md:text-lg">
+            <tr className="h-[74px] border-t border-t-[#5f6e82] md:h-14">
+                <td className="show992 hidden hover:bg-[#384459] md:table-cell">#341</td>
+                <td className="show576 hidden hover:bg-[#384459] md:table-cell">08/07/2022</td>
+                <td className="hover:bg-[#384459]">Anakin</td>
+                <td className="show992 hidden hover:bg-[#384459] md:table-cell">15</td>
+                <td className="show992 hidden hover:bg-[#384459] md:table-cell">11</td>
+                <td className="hover:bg-[#384459]">R$ 55300.00</td>
+
+                <td className="flex justify-center items-center mt-3 md:mt-1">
+                    <NotificationButton />
+                </td>
+            </tr>
+
+        </tbody>
+    );
+}
