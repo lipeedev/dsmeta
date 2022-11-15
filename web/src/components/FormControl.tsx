516f5d69d1b6e38adbf93a816603bc4bb4e9f430
1Leep <anamariafakezaum@gmail.com>
2022-11-14 21:12:40 -0300

    feat: add Form Control Component

---
+++ web/src/components/FormControl.tsx
@@ -0,0 +1,9 @@
+export function FormControl() {
+    return (
+        <div>
+            <div className="mb-4 max-w-[300px]">
+                <input className="w-full h-[46px] bg-[#1b2531] border border-[#384459] rounded-md text-[#9aaabe] pt-0 px-5 text-lg" type="text" />
+            </div>
+        </div>
+    );
+}
--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -1,2 +1,4 @@
 export * from './NotificationButton';
 export * from './Heading';
+export * from './SalesCard';
+export * from './FormControl';
