548e86c05c3ae8a211dbf82b85887f0f64c0df60
1Leep <anamariafakezaum@gmail.com>
2022-11-14 22:27:17 -0300

    fix: shows columns on correct screen size

--- web/src/components/SalesTableTHead.tsx
+++ web/src/components/SalesTableTHead.tsx
@@ -2,11 +2,11 @@ export function SalesTableTHead() {
     return (
         <thead className="h-14 text-[#e9e9e9] font-bold">
             <tr>
-                <th className="show992 hidden md:table-cell">ID</th>
+                <th className="show992 hidden lg:table-cell">ID</th>
                 <th className="show576 hidden md:table-cell ">Data</th>
                 <th>Vendedor</th>
-                <th className="show992 hidden md:table-cell">Visitas</th>
-                <th className="show992 hidden md:table-cell">Vendas</th>
+                <th className="show992 hidden lg:table-cell">Visitas</th>
+                <th className="show992 hidden lg:table-cell">Vendas</th>
                 <th>Total</th>
                 <th>Notificar</th>
             </tr>
